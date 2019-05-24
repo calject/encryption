3199b8b75ffd99e47fac3baf78e35cb81c13a44e
沧澜 <lirongyi@51ucar.cn>
2019-05-24 16:32:30 +0800

    change aes def opts

--- src/Providers/AES/AES.php
+++ src/Providers/AES/AES.php
@@ -22,9 +22,9 @@ class AES extends AbsAesEncryption
      * @param mixed $opts
      * @return string
      */
-    public function encrypt(string $str, $opts = OPENSSL_RAW_DATA): string
+    public function encrypt(string $str, $opts = null): string
     {
-        return $this->coding()->encode(openssl_encrypt($str, $this->cipherMode, $this->padding()->padding($this->key), $opts, $this->iv));
+        return $this->coding()->encode(openssl_encrypt($str, $this->cipherMode, $this->padding()->padding($this->key), $opts ?? OPENSSL_RAW_DATA, $this->iv));
     }
     
     /**
@@ -32,9 +32,9 @@ class AES extends AbsAesEncryption
      * @param mixed $opts
      * @return string
      */
-    public function decrypt(string $str, $opts = OPENSSL_RAW_DATA): string
+    public function decrypt(string $str, $opts = null): string
     {
-        return openssl_decrypt($this->coding()->decode($str), $this->cipherMode, $this->padding()->padding($this->key), $opts, $this->iv);
+        return openssl_decrypt($this->coding()->decode($str), $this->cipherMode, $this->padding()->padding($this->key), $opts ?? OPENSSL_RAW_DATA, $this->iv);
     }
     
 }
